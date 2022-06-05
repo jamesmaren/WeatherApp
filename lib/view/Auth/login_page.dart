d83343ee7c3695874807dc81152c09ff0b4f72f7
Marenya <marenyaomondi@gmail.com>
2022-06-05 17:51:46 +0300

    login page done

--- lib/view/Auth/login_page.dart
+++ lib/view/Auth/login_page.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+    bool _isObscure = true;
     return Scaffold(
-        body: Column(
-      children: [
+        body: SingleChildScrollView(
+      child: Column(children: [
         Container(
-          height: MediaQuery.of(context).size.height * 0.20,
+          height: MediaQuery.of(context).size.height * 0.19,
           width: MediaQuery.of(context).size.width * 0.86,
-          margin: const EdgeInsets.only(top: 55),
+          margin: const EdgeInsets.only(top: 75),
           decoration: const BoxDecoration(
             image: DecorationImage(
               alignment: Alignment.topCenter,
@@ -22,7 +28,7 @@ class LoginPage extends StatelessWidget {
           ),
         ),
         const SizedBox(
-          height: 30,
+          height: 15,
         ),
         const Text(
           "Login",
@@ -34,7 +40,7 @@ class LoginPage extends StatelessWidget {
           ),
         ),
         const SizedBox(
-          height: 20,
+          height: 10,
         ),
         Container(
           width: MediaQuery.of(context).size.width * 0.89,
@@ -54,7 +60,7 @@ class LoginPage extends StatelessWidget {
                 hintText: "Email",
                 prefixIcon: const Icon(
                   Icons.email,
-                  color: Colors.deepOrangeAccent,
+                  color: Colors.lightBlueAccent,
                 ),
                 focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
@@ -69,7 +75,7 @@ class LoginPage extends StatelessWidget {
           ),
         ),
         const SizedBox(
-          height: 15,
+          height: 20,
         ),
         Container(
           width: MediaQuery.of(context).size.width * 0.89,
@@ -85,11 +91,12 @@ class LoginPage extends StatelessWidget {
               ]),
           child: TextField(
             //controller: emailController,
+            obscureText: _isObscure,
             decoration: InputDecoration(
                 hintText: "Password",
                 prefixIcon: const Icon(
                   Icons.password,
-                  color: Colors.deepOrangeAccent,
+                  color: Colors.lightBlueAccent,
                 ),
                 focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
@@ -100,30 +107,140 @@ class LoginPage extends StatelessWidget {
                     borderSide:
                         const BorderSide(color: Colors.white, width: 1.0)),
                 border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30))),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    })),
           ),
         ),
         const SizedBox(
-          height: 5,
+          height: 10,
         ),
         Row(
           //crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.end,
           children: const [
-            Text(
-              "Forgot Password?",
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.grey,
-                fontWeight: FontWeight.bold,
+            Padding(
+              padding: EdgeInsets.only(right: 18.0, top: 3),
+              child: Text(
+                "Forgot Password?",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                ),
               ),
             ),
           ],
         ),
         const SizedBox(
-          height: 20,
+          height: 25,
         ),
-      ],
+        TextButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+              shadowColor: MaterialStateProperty.all(Colors.blueGrey),
+              elevation: MaterialStateProperty.all(15),
+              padding: MaterialStateProperty.all(const EdgeInsets.all(15)),
+              minimumSize: MaterialStateProperty.all(const Size(195, 50)),
+              shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30))),
+            ),
+            child: const Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 23,
+                letterSpacing: 1,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+        const SizedBox(
+          height: 13,
+        ),
+        Container(
+          padding: const EdgeInsets.only(left: 50),
+          child: Row(
+            children: [
+              const Text(
+                "Don/'t have an account",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 15,
+                    ),
+                  ))
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.only(right: 18.0, top: 3),
+              child: Text(
+                "---Or login with ---",
+                style: TextStyle(
+                  fontSize: 18,
+                  letterSpacing: 1,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 13,
+        ),
+        Row(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: const [
+                Image(
+                  image: AssetImage('images/gmail.png'),
+                  // height: 50,
+                  // width: 50,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            // Column(
+            //   children: const [
+            //     Image(
+            //       image: AssetImage('images/facebook.png'),
+            //       // height: 50,
+            //       // width: 50,
+            //     ),
+            //   ],
+            // ),
+          ],
+        )
+      ]),
     ));
   }
 }
